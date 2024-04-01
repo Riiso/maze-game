edc0ec4e9d0f4e947e9f4fe70b0cc03b28009a4c
risoslatarovic@gmail.com <risoslatarovic@gmail.com>
2024-04-01 20:04:23 +0200

    perf: enhance A* algorithm with early termination condition

--- src/utils.py
+++ src/utils.py
@@ -2,9 +2,7 @@ import heapq
 import time
 import threading
 
-class Node:
-    """A node class for A* Pathfinding"""
-
+class Node: # A class to represent a node
     def __init__(self, parent=None, position=None):
         self.parent = parent
         self.position = position
@@ -12,100 +10,77 @@ class Node:
         self.h = 0
         self.f = 0
 
-    def __eq__(self, other):
-        # Override the default equals method
+    def __eq__(self, other):    # Override the default equals method
         return self.position == other.position
 
-    def __lt__(self, other):
-        # For the heap queue to work with the lowest f value, we need to override the less than operator
+    def __lt__(self, other):    # For the heap queue to work with the lowest f value, we need to override the less than operator
         return self.f < other.f
     
-    def __hash__(self):
-        # Hash based on the node's unique position
+    def __hash__(self): # Hash based on the node's unique position
         return hash(self.position)
 
 def astar_threaded(maze, start, collectibles, end, callback):
     def run_astar():
-        # Run the original astar function
-        path = astar_collectibles(maze, start, collectibles, end)
-        # Call the callback with the result
-        callback(path)
+        path = astar_collectibles(maze, start, collectibles, end)   # Run the original astar function
+        callback(path)  # Call the callback with the result
 
-    # Start the astar function in a new thread
-    threading.Thread(target=run_astar).start()
+    threading.Thread(target=run_astar).start()  # Start the astar function in a new thread
 
 def dijkstra_threaded(maze, start, collectibles, end, callback):
     def run_dijkstra():
-        # Run the original dijkstra function
-        path = dijkstra(maze, start, collectibles, end)
-        # Call the callback with the result
-        callback(path)
+        path = dijkstra(maze, start, collectibles, end) # Run the original dijkstra function
+        callback(path)  # Call the callback with the result
 
-    # Start the dijkstra function in a new thread
-    threading.Thread(target=run_dijkstra).start()
+    threading.Thread(target=run_dijkstra).start()   # Start the dijkstra function in a new thread
 
-def astar(maze, start, end, start_time):
-    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+def astar(maze, start, end, start_time, shortest_path_so_far=float('inf')):    # A* algorithm to find path from start to end
 
-    # Create start and end node
     start_node = Node(None, start)
     start_node.g = start_node.h = start_node.f = 0
     end_node = Node(None, end)
     end_node.g = end_node.h = end_node.f = 0
 
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
+    open_list = []  # For nodes to be evaluated
+    closed_list = []    # Nodes already evaluated
 
-    # Heapify the open_list and Add the start node
-    heapq.heapify(open_list) 
+    heapq.heapify(open_list)
     heapq.heappush(open_list, start_node)
 
-    # Loop until you find the end
-    while len(open_list) > 0:
+    while len(open_list) > 0:   # Loop until you find the end
 
         # Get the current node
         current_node = heapq.heappop(open_list)
         closed_list.append(current_node)
 
-        # Found the goal
-        if current_node == end_node:
+        if current_node == end_node:    # Path found
             path = []
             current = current_node
-            while current is not None:
+            while current is not None:  # Trace back the path
                 path.append(current.position)
                 current = current.parent
             return path[::-1]  # Return reversed path
 
-        # Generate children
         children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:  # Adjacent squares
-            if time.time() - start_time > 5:  # Limit search time to 1 second
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:  # Generate children
+            if time.time() - start_time > 5:  # Limit search time to 5 second
                 print("A* time limit exceeded, returning part of path.")
                 return []  # Return None for the target position and an empty list for the path if time limit is exceeded    
 
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])    # Get node position
 
-            # Make sure within range
-            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:    # Make sure within range
                 continue
 
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] not in [0, 3, 4, 5, 6, 7]:
+            if maze[node_position[0]][node_position[1]] not in [0, 3, 4, 5, 6, 7]:  # Make sure walkable terrain
                 continue
 
-            # Create new node
-            new_node = Node(current_node, node_position)
+            new_node = Node(current_node, node_position)  # Create new node
 
-            # Append
             children.append(new_node)
 
-        # Loop through children
         for child in children:
 
-            # Child is on the closed list
-            if child in closed_list:
+            if child in closed_list:    # Child is on the closed list
                 continue
 
             # Create the f, g, and h values
@@ -113,60 +88,50 @@ def astar(maze, start, end, start_time):
             child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
             child.f = child.g + child.h
 
-            # Child is already in the open list
-            if len([open_node for open_node in open_list if child == open_node and child.g > open_node.g]) > 0:
+            # Early break if the current path cost exceeds the shortest path found so far
+            if child.f > shortest_path_so_far:
+                print("A* path cost exceeded shortest path found so far.")
+                continue  # Skip this child as it cannot possibly lead to a shorter path
+
+            if len([open_node for open_node in open_list if child == open_node and child.g > open_node.g]) > 0: # Child is already in the open list and has a higher g value
                 continue
 
-            # Add the child to the open list
-            heapq.heappush(open_list, child)
+            heapq.heappush(open_list, child)    # Add the child to the open list
 
     return None  # No path found
 
-def astar_collectibles(maze, start, collectibles, end):
+def astar_collectibles(maze, start, collectibles, end): # A* algorithm to find path to collectibles and end
     start_time = time.time()  # Start timing
     path = []
     current_position = start
 
-    # Ensure collectibles are tuples, since your astar uses tuple positions
-    collectibles = [tuple(collectible) for collectible in collectibles]
+    collectibles = [tuple(collectible) for collectible in collectibles] # Ensure collectibles are tuples
 
     while len(collectibles) > 0:
-        # Find closest collectible
-        closest_collectible, closest_path = find_closest_collectible(maze, current_position, collectibles, start_time)
+        closest_collectible, closest_path = find_closest_collectible(maze, current_position, collectibles, start_time)  # Find closest collectible
         if closest_path:
-            # Exclude the starting point of each segment after the first to avoid duplicate positions
-            path += closest_path if not path else closest_path[1:]
+            path += closest_path if not path else closest_path[1:]  # Exclude the starting point of each segment after the first to avoid duplicate positions
             current_position = closest_collectible
             collectibles.remove(closest_collectible)
         else:
-            #print("Could not find path to collectible:", closest_collectible)
             break  # Break the loop if no path to a collectible is found
-
-    # If current position is not the end, find path from current position to end
-    if current_position != end:
-        print("Finding path from current position to end:", current_position, end)
+  
+    if current_position != end: # If current position is not the end, find path from current position to end
         final_path = astar(maze, current_position, end, start_time)
         if final_path:
-            print("Found path from current position to end:", final_path)
-            # Exclude the starting point of the final segment to avoid duplicate positions
-            path += final_path[1:]  # Ensure we're not duplicating the last collectible's position
-        #else:
-            #print("Could not find path from current position to end.")
-    print("Final path:", path)
+            path += final_path[1:]  # Exclude the starting point of the final segment to avoid duplicate positions
     elapsed_time = time.time() - start_time  # Calculate elapsed time
     print(f"A* found path in {elapsed_time:.5f} seconds.")
     return path
 
-def find_closest_collectible(maze, start, collectibles, start_time):
+def find_closest_collectible(maze, start, collectibles, start_time):    # Find the closest collectible to the start position
     closest_collectible = None
     closest_path = []
-    shortest_distance = float('inf')
+    shortest_distance = float('inf')    # Initialize shortest distance to infinity
 
     for collectible in collectibles:
-        print("Trying to find path from", start, "to collectible at", collectible)
-        path = astar(maze, start, collectible, start_time)
+        path = astar(maze, start, collectible, start_time, shortest_distance)
         if path:
-            print("Found path to collectible", collectible, ":", path)
             if len(path) < shortest_distance:
                 closest_collectible = collectible
                 closest_path = path
@@ -174,23 +139,24 @@ def find_closest_collectible(maze, start, collectibles, start_time):
         else:
             print("No path found to collectible", collectible)
 
-    #if closest_collectible is None:
-        #print("No closest collectible found. This could be due to maze configuration or collectibles' positioning.")
+    if closest_collectible is None:
+        print("No closest collectible found. This could be due to maze configuration or collectibles' positioning.")
+    
     return closest_collectible, closest_path
 
-# ------------------------------------------------------------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def dijkstra_path(maze, start, targets, start_time):
+def dijkstra_path(maze, start, targets, start_time):    # Dijkstra algorithm to find path from start to any target  
     start_node = Node(None, start)
     start_node.g = 0
 
-    open_list = []
-    heapq.heappush(open_list, (0, start_node))
+    open_list = []  # For nodes to be evaluated
+    heapq.heappush(open_list, (0, start_node))  # Add the start node to the open list
 
-    visited = set()
+    visited = set() # Nodes already evaluated
 
-    while open_list:
-        current_g, current_node = heapq.heappop(open_list)
+    while open_list:    # Loop until you find the target
+        current_g, current_node = heapq.heappop(open_list)  # Get the current node
 
         if current_node.position in targets:
             path = []
@@ -200,30 +166,30 @@ def dijkstra_path(maze, start, targets, start_time):
                 current_node = current_node.parent
             return target_position, path[::-1]  # Return both the target position and the path
 
-        visited.add(current_node.position)
+        visited.add(current_node.position)  # Add the current node to the visited set
 
-        for direction in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-            if time.time() - start_time > 5:  # Limit search time to 1 second
+        for direction in [(0, -1), (0, 1), (-1, 0), (1, 0)]:    # Generate children
+            if time.time() - start_time > 5:  # Limit search time to 5 second
                 print("Dijkstra time limit exceeded, try A* instead.")
                 return None, []  # Return None for the target position and an empty list for the path if time limit is exceeded
             neighbor_pos = (current_node.position[0] + direction[0], current_node.position[1] + direction[1])
-            if 0 <= neighbor_pos[0] < len(maze) and 0 <= neighbor_pos[1] < len(maze[0]) and maze[neighbor_pos[0]][neighbor_pos[1]] in [0, 3, 4, 5, 6, 7] and neighbor_pos not in visited:
+            if 0 <= neighbor_pos[0] < len(maze) and 0 <= neighbor_pos[1] < len(maze[0]) and maze[neighbor_pos[0]][neighbor_pos[1]] in [0, 3, 4, 5, 6, 7] and neighbor_pos not in visited:   # Make sure within range and walkable terrain
                 neighbor_node = Node(current_node, neighbor_pos)
                 neighbor_g = current_g + 1
                 heapq.heappush(open_list, (neighbor_g, neighbor_node))
 
     return None, []  # Return None for the target position and an empty list for the path if no path was found
 
-def dijkstra(maze, start, collectibles, end):
+def dijkstra(maze, start, collectibles, end):   # Dijkstra algorithm to find path to collectibles and end
     start_time = time.time()  # Start timing
     path = []
     current_position = start
     targets = set(collectibles + [end])  # Use a set for efficient look-up
 
-    while targets:
-        target_position, shortest_path = dijkstra_path(maze, current_position, targets, start_time)
+    while targets:  # Loop until all targets are found
+        target_position, shortest_path = dijkstra_path(maze, current_position, targets, start_time) # Find the shortest path to any target
         if shortest_path:
-            path += shortest_path if not path else shortest_path[1:]
+            path += shortest_path if not path else shortest_path[1:]    # Exclude the starting point of each segment after the first to avoid duplicate positions
             current_position = target_position
             targets.discard(target_position)  # Remove the found target
         else:
@@ -232,40 +198,3 @@ def dijkstra(maze, start, collectibles, end):
     elapsed_time = time.time() - start_time  # Calculate elapsed time
     print(f"Dijkstra found path in {elapsed_time:.5f} seconds.")
     return path
-
-'''
-#Example usage
-maze = [
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 5, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1],
-            [1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 3, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1],
-            [1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1],
-            [1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
-            [1, 1, 0, 3, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1],
-            [1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 3, 0, 1, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1],
-            [1, 1, 1, 1, 0, 5, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 3, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 5, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-]
-
-start = (22, 32)
-end = (19, 33)
-
-path = astar(maze, start, end)
-print(path)
-'''
